1d016f6ad1e0f6651f642dcfbc49871a665fa0f4
Evans Gacheru <evans.gacheru.munene@gmail.com>
2026-09-10 13:53:03 +0300

    config updates

--- web/next.config.ts
+++ web/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: "/dck-care ",
+  output: "export",
+  trailingSlash: true,
 };
 
 export default nextConfig;
